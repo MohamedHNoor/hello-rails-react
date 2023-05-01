07dc51c36467198a0e51da47e25e102476008191
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-05-01 23:27:01 +0200

    Fix rubocop errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,7 +3,7 @@ module Api
     class MessagesController < ApplicationController
       def index
         greeting = Message.find(rand(1..Message.count))
-        render json: { message: greeting.text}
+        render json: { message: greeting.text }
       end
     end
   end
